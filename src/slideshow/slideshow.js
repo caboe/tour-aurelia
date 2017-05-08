4b130aaebe46ed29d5263fcc22a178730b9de44b
caboe <ca_boe@yahoo.de>
2017-05-08 22:35:41 +0200

    styling

--- src/slideshow/slideshow.js
+++ src/slideshow/slideshow.js
@@ -1,3 +1,4 @@
 export class Slideshow{
 
+    //https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id=" + setId + "&api_key=531e7a0d62fe823d91b9ebcfca750195&format=json&jsoncallback=tour.images.fillStageIndex
 }
